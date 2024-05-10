e3f92166f2eaf11b72965d7b0fd5fd9c91297f66
QuispeAbel <quisperobledoabel@gmail.com>
2024-05-10 18:50:38 -0300

    Clase Jugador

--- src/main/java/poo/Jugador.java
+++ src/main/java/poo/Jugador.java
@@ -1,5 +1,17 @@
 package poo;
 
 public class Jugador {
+    int Puntos = 0;
+
+    Jugador() {
+    }
+
+    public int getPuntos() {
+        return Puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        Puntos = puntos;
+    }
 
 }
