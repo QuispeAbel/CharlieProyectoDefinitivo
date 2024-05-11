dbf1e5b13d04f44772bc724dca9cc538e74e19cf
QuispeAbel <quisperobledoabel@gmail.com>
2024-05-10 22:38:38 -0300

    color a launcher y saque el menu de arriba

--- src/main/java/poo/LanzadorJuegos.java
+++ src/main/java/poo/LanzadorJuegos.java
@@ -32,28 +32,7 @@ public class LanzadorJuegos extends JPanel implements ActionListener {
         int filas = 0;
         int columnas = 2;
         int separacion = 10;
-        String[] arrComponentes = { "circus charlie", "pong" };
         JFrame f = new JFrame("Lanzador");
-
-        /* Creacion de la Barra de Menu y los items de menu */
-        MenuBar mBar = new MenuBar();
-        f.setMenuBar(mBar);
-
-        Menu menuArchivo = new Menu("Archivo");
-        Menu menuComponentes = new Menu("juegos");
-
-        mBar.add(menuArchivo);
-        menuArchivo.add(new MenuItem("Salir"));
-        menuArchivo.addActionListener(this);
-        menuArchivo.add(new MenuItem("configuracion"));
-
-        // Menu Componentes
-        mBar.add(menuComponentes);
-        for (String compo : arrComponentes) {
-            MenuItem menuItem = new MenuItem(compo);
-            menuComponentes.add(menuItem);
-        }
-        menuComponentes.addActionListener(this);
 
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         boton2 = new JButton(
@@ -65,9 +44,12 @@ public class LanzadorJuegos extends JPanel implements ActionListener {
         setLayout(new BorderLayout());
 
         PanelTitulo = new JPanel();// Panel para el titulo de la Ventana
-        PanelTitulo.add(new JLabel("SELECCIONE SU JUEGO"));
+        Color violeta = new Color(153, 113, 225);
+        PanelTitulo.setBackground(violeta);
+        PanelTitulo.add(new JLabel(new ImageIcon(getClass().getResource("imagenes/OIG3.jpg"))));
 
         PanelDelCentro = new JPanel();// PAnel con los botones
+        PanelDelCentro.setBackground(violeta);
         PanelDelCentro.setLayout(new GridLayout(filas, columnas, separacion, separacion));
 
         boton2.setBackground(Color.BLACK);// hago el fondo del boton negro
@@ -79,8 +61,8 @@ public class LanzadorJuegos extends JPanel implements ActionListener {
         PanelDelCentro.add(boton2);
         PanelDelCentro.add(boton3);
 
-        add(PanelDelCentro, BorderLayout.CENTER);
-        add(PanelTitulo, BorderLayout.NORTH);
+        f.add(PanelDelCentro, BorderLayout.CENTER);
+        f.add(PanelTitulo, BorderLayout.NORTH);
 
         WindowListener l = new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -88,7 +70,7 @@ public class LanzadorJuegos extends JPanel implements ActionListener {
             };
         };
 
-        f.add(PanelDelCentro);
+        // f.add(PanelDelCentro);
 
         f.addWindowListener(l);
         f.pack();
@@ -97,72 +79,6 @@ public class LanzadorJuegos extends JPanel implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-
-        String cmd = e.getActionCommand();
-
-        if (cmd == "Salir") {
-            System.exit(0); // Sale del programita
-        }
-
-        switch (cmd) {
-            case "circus charlie":
-                juego = new Pong();
-                t = new Thread() {
-                    public void run() {
-                        juego.run(1.0 / 60.0);
-                    }
-                };
-                t.start();
-                break;
-            case "pong":
-                juego = new DemoJuego03();
-
-                t = new Thread() {
-                    public void run() {
-                        juego.run(1.0 / 60.0);
-                    }
-                };
-                t.start();
-                break;
-            case "configuracion":
-                System.out.println("configuracion");
-                Config = new Frame("configuracion");
-                Config.setSize(640, 480);
-                Config.setVisible(true);
-
-                JPanel teclas = new JPanel();
-                teclas.setLayout(new GridLayout(0, 2, 10, 10));
-                teclas.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-                Label avanzar = new Label("avanzar:");
-                Label retroceder = new Label("retroceder:");
-                Label arriba = new Label("arriba:");
-                Label abajo = new Label("abajo:");
-                Label saltar = new Label("saltar:");
-
-                teclas.add(avanzar);
-                teclas.add(new TextField("d"));
-                teclas.add(retroceder);
-                teclas.add(new TextField("a"));
-                teclas.add(arriba);
-                teclas.add(new TextField("w"));
-                teclas.add(abajo);
-                teclas.add(new TextField("s"));
-                teclas.add(saltar);
-                teclas.add(new TextField("space"));
-
-                Config.add(teclas);
-                Config.pack();
-                Config.setLocationRelativeTo(null);
-
-                Config.addWindowListener(new WindowAdapter() {
-                    public void windowClosing(WindowEvent windowEvent) {
-                        Config.setVisible(false);
-                    }
-                });
-
-                break;
-        }
 
         if (e.getSource() == boton2) {
             juego = new Pong();
